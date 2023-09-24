8a2ee06f34a7591166e25813c5a1149e0e1a507b
Maksim Marchukov <marchukov@uchi.ru>
2023-09-24 13:58:40 +0500

    Class for game clients

---
+++ server/GameClients.js
@@ -0,0 +1,38 @@
+class GameClients {
+  constructor() {
+    this.clients = {};
+  }
+
+  addClient(clientName, client) {
+    this.clients[clientName] = client;
+  }
+
+  removeClient(clientName) {
+    delete this.clients[clientName];
+  }
+
+  getClient(name) {
+    return this.clients[name];
+  }
+
+  sendToAll(message) {
+    this.forEachClient((client) => client.send(message));
+  }
+
+  sendTo(name, message) {
+    const client = this.getClient(name);
+    if (client) client.send(message);
+  }
+
+  get length() {
+    return Object.keys(this.clients).length;
+  }
+
+  forEachClient(callback) {
+    for (const id in this.clients) {
+      callback(this.clients[id]);
+    }
+  }
+}
+
+export const gameClients = new GameClients();
